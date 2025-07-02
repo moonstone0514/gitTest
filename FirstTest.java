eece48961d5a337f3601bbcb7f448f199be74a11
kddmmm <ehdals5387@naver.com>
2025-07-02 17:22:28 +0900

    김동민

---
+++ FirstTest.java
@@ -0,0 +1,26 @@
+//컴파일(저장시 자동 컴파일): ctrl+s
+//실행: ctrl+f11
+
+//컴파일 명령어: java FirstTest.java
+//실행 명령어: java FirstTest
+
+package step01; //1번만 선언
+
+//import 선언구: 여러번 가능
+
+/*
+ * 	class 프로그램명{
+ * 		데이터 표현: 변수
+ * 		생성자: 메모리상에 실체화시키는 기능
+ * 		기능: 메소드
+ * 		static{}: 호출 없이도 단 1회 자동 실행
+ * 	}
+ */
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+
+}
